7ef4642c455a30e8a98145ae3f014a05b01e0cdc
Abhishek Patharia <warmachine7750@gmail.com>
2022-07-23 23:25:26 +0530

    New: Added linkedin-clone landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import  { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+
 import './App.css';
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login/>}></Route>,
+          <Route path="/home" element={<> <Header/> <Home/> </>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Header = (props) => {
+    return (
+        <Container>
+            <Content></Content>
+        </Container>
+    );
+}
+
+const Container = styled.div``;
+
+const Content = styled.div`
+
+`;
+
+export default Header;
